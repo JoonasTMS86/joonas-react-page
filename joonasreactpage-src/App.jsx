17cc554ab22f368352d0fe7b99cc1899b805e8ff
Joonas Lindberg <joonaslindberg@hotmail.com>
2026-02-16 21:33:21 +0200

    Created a form with which you can add new forum thread titles & define how many replies they have

--- joonasreactpage-src/App.jsx
+++ joonasreactpage-src/App.jsx
@@ -1,67 +1,83 @@
 import React from 'react';
+import { useState } from 'react';
 
-const Part = (props) => {
+const ForumTopics = (props) => {
   return (
     <div>
-      {props.reactTopic.threadTitle} {props.reactTopic.replies}
+      {props.forumTopics.map(forumTopic => <li key={forumTopic.id}>{forumTopic.threadTitle}, {forumTopic.replies} replies</li>)}
     </div>
   )
 }
 
-const Content = (props) => {
-
-const reactTopics = props.reactTopic.threads
-
-  return (
-    <div>
-      {reactTopics.map(reactTopic => <Part key={reactTopic.id} reactTopic={reactTopic}/>)}
-    </div>
-  )
-}
-
-const Header = (props) => {
-  return (
-      <h1>
-        {props.name}
-      </h1>
-  )
-}
-
-const ReactTopics = (props) => {
-  return (
-    <div>
-      <Header name={props.reactTopic.name} />
-      <Content reactTopic={props.reactTopic} />
-    </div>
-  )
-}
+const forumThreads =
+[
+  {
+    threadTitle: 'Neat Stuff to Do with React',
+    replies: 33,
+    id: 1
+  },
+  {
+    threadTitle: 'The Many Things You Can Do with JavaScript',
+    replies: 25,
+    id: 2
+  },
+  {
+    threadTitle: 'What PHP Can Be Used For',
+    replies: 10,
+    id: 3
+  }
+]
 
 const App = () => {
-  const reactTopic = {
-    name: 'Joonas\' Own React Page',
-    id: 1,
-    threads: [
-      {
-        threadTitle: 'Neat Stuff to Do with React',
-        replies: 33,
-        id: 1
-      },
-      {
-        threadTitle: 'The Many Things You Can Do with JavaScript',
-        replies: 25,
-        id: 2
-      },
-      {
-        threadTitle: 'What PHP Can Be Used For',
-        replies: 10,
-        id: 3
+
+  const [topics, setTopics] = useState(forumThreads)
+  const [newTopic, setNewTopic] = useState('')
+  const [howManyReplies, setHowManyReplies] = useState(0)
+
+  const addTopic = (event) => {
+    event.preventDefault()
+
+    if(newTopic.trim() != "") {
+      const noteObject = {
+        threadTitle: newTopic,
+        replies: howManyReplies,
+        id: String(topics.length + 1),
       }
-    ]
+
+      setTopics(topics.concat(noteObject))
+      setNewTopic('')
+    }
+  }
+
+  const handleAddTopic = (event) => {
+    setNewTopic(event.target.value)
+  }
+
+  const handleHowManyReplies = (event) => {
+    setHowManyReplies(event.target.value)
   }
 
   return (
     <div>
-      <ReactTopics reactTopic={reactTopic} />
+      <ForumTopics forumTopics={topics} />
+      
+      <br/>
+      <br/>
+      <form onSubmit={addTopic}>
+        Thread title:
+        <input 
+        value={newTopic} 
+        onChange={handleAddTopic}
+        />
+        <br/>
+        How many replies:
+        <input 
+        value={howManyReplies} 
+        onChange={handleHowManyReplies}
+        />
+        <br/>
+        <button type="submit">Add Topic</button>
+      </form>   
     </div>
   )
 }
